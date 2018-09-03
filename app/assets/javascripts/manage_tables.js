53359a9b3ecaeb8c33dd00166ba70b18f918a38e
Nelson Chan <chakflying@hotmail.com>
2018-09-03 17:52:14 +0800

    Adjusted Datatables responsive layout

--- app/assets/javascripts/manage_tables.js
+++ app/assets/javascripts/manage_tables.js
@@ -2,7 +2,19 @@ var dataTable = null
 
 document.addEventListener("turbolinks:load", function() {
     if ( ! $.fn.DataTable.isDataTable( '.manage-table' ) ) {
-        $('.manage-table').dataTable({stateSave: true});
+        $('.manage-table').dataTable({
+            stateSave: true,
+            responsive: true,
+            columnDefs: [
+                { responsivePriority: 2, targets: 2 },
+                { responsivePriority: 1, targets: -1 },
+                {'width': '5%', 'targets': 0},
+                {'width': '17%', 'targets': 1},
+                {'width': '10%', 'targets': 2},
+                {'width': '35%', 'targets': -2},
+                {'width': '57px', 'targets': -1},
+            ]
+        });
     }
 });
 
