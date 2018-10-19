b325263642168b3080c573853288dc41fb8d4506
Nelson Chan <chakflying@hotmail.com>
2018-10-20 02:31:55 +0800

    Fix syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    include ApplicationHelper
     include SessionsHelper
     include UsersHelper
     include StudentsHelper
 
     before_action :set_last_seen_at, if: proc { logged_in? && (current_user.last_seen_at.nil? || current_user.last_seen_at < 15.minutes.ago) }
-    before_filter :reset_hsts
+    before_action :reset_hsts
 end
