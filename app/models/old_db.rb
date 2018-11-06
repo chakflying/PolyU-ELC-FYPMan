871d870607d087ce0d9442d4343d205ef08d3c3c
Nelson Chan <chakflying@hotmail.com>
2018-11-06 20:48:57 +0800

    Another total derp

--- app/models/old_db.rb
+++ app/models/old_db.rb
@@ -1,6 +1,6 @@
 class OldDb < ActiveRecord::Base  
     # self.abstract_class = true
-    establish_connection "production_old"
+    establish_connection "production_old".to_sym
 
     def self.sync
         OldDb.all.each do |record|
