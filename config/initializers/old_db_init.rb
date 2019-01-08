c18ec88a89246df8fbad9e28ae7c805076ec3c69
Nelson Chan <chakflying@hotmail.com>
2019-01-08 18:24:49 +0800

    better naming and timestamps for Old Db models

--- config/initializers/old_db_init.rb
+++ config/initializers/old_db_init.rb
@@ -3,6 +3,10 @@ class OldDepartments < Sequel::Model(Old_DB[:departments]); end
 class OldRelations < Sequel::Model(Old_DB[:supervises]); end
 class OldTodos < Sequel::Model(Old_DB[:todos]); end
 
+OldUsers.plugin :timestamps, create: :date_created, update: :date_modified
+OldRelations.plugin :timestamps, create: :date_created, update: :date_modified
+OldTodos.plugin :timestamps, create: :date_created, update: :date_modified
+
 class OldDbSyncTask
     include Delayed::RecurringJob
     run_every 5.minute
