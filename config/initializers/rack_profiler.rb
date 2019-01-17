8dc0bb00e9d540fbc6be7b30aa28ffbed836ebb2
Nelson Chan <chakflying@hotmail.com>
2019-01-17 20:00:12 +0800

    Caching

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -6,4 +6,5 @@ if Rails.env.development? or Rails.env.production?
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
   Rack::MiniProfiler.config.authorization_mode = :whitelist
+  Rack::MiniProfiler.config.disable_caching = false if Rails.env.production?
 end
