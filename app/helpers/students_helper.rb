119bfe8a7e3ab498f7651614492c48426d5e030c
Nelson Chan <chakflying@hotmail.com>
2019-01-18 00:09:22 +0800

    FYP years start from last year

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -3,6 +3,7 @@
 module StudentsHelper
   def get_fyp_years_list
     [
+      [(Time.now.year - 1).to_s + '-' + Time.now.year.to_s, (Time.now.year - 1).to_s + '-' + Time.now.year.to_s],
       [Time.now.year.to_s + '-' + (Time.now.year + 1).to_s, Time.now.year.to_s + '-' + (Time.now.year + 1).to_s],
       [(Time.now.year + 1).to_s + '-' + (Time.now.year + 2).to_s, (Time.now.year + 1).to_s + '-' + (Time.now.year + 2).to_s],
       [(Time.now.year + 2).to_s + '-' + (Time.now.year + 3).to_s, (Time.now.year + 2).to_s + '-' + (Time.now.year + 3).to_s],
