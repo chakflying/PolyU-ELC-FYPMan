f17e1a8e1d65771230edc3280fc5e8ac55aaa5db
Nelson Chan <chakflying@hotmail.com>
2019-01-17 20:42:57 +0800

    Profiler not used anyway because of CSP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   before_action :set_last_seen_at, if: proc { logged_in? && (current_user.last_seen_at.nil? || current_user.last_seen_at < 15.minutes.ago) }
   before_action :set_paper_trail_whodunnit
   after_action :store_location
-  before_action :show_profiler
+  # before_action :show_profiler
 
   def render_404
     raise ActionController::RoutingError, 'Not Found'
--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-if Rails.env.development? or Rails.env.production?
+if Rails.env.development?
   require "rack-mini-profiler"
 
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
   Rack::MiniProfiler.config.authorization_mode = :whitelist
-  Rack::MiniProfiler.config.disable_caching = false if Rails.env.production?
 end
