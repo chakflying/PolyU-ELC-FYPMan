b0be06cd7db18908a275250f5360ca71ef63a618
Nelson Chan <chakflying@hotmail.com>
2019-01-19 16:48:07 +0800

    early return and code formatting

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,65 +5,69 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import Assign from '../assign.vue'
-import Timeline from '../timeline.vue'
-import TurbolinksAdapter from 'vue-turbolinks';
+import Vue from "vue";
+import VueResource from "vue-resource";
+import Assign from "../assign.vue";
+import Timeline from "../timeline.vue";
+import TurbolinksAdapter from "vue-turbolinks";
 
-Vue.use(TurbolinksAdapter)
-Vue.use(VueResource)
+Vue.use(TurbolinksAdapter);
+Vue.use(VueResource);
 
+document.addEventListener("turbolinks:load", () => {
+  var element = document.getElementById("assign-form-vue");
+  if (element == null) return;
+  const parsed_props = JSON.parse(element.getAttribute("data"));
+  var student_dropdown_list = parsed_props.students.map(function(a) {
+    return { id: a.netID, text: a.netID + "  -  " + a.name };
+  });
+  var supervisor_dropdown_list = parsed_props.supervisors.map(function(a) {
+    return { id: a.netID, text: a.netID + "  -  " + a.name };
+  });
+  const props = {
+    students: student_dropdown_list,
+    supervisors: supervisor_dropdown_list
+  };
 
-document.addEventListener('turbolinks:load', () => {
-    var element = document.getElementById("assign-form-vue")
-    if (element == null) return;
-    const parsed_props = JSON.parse(element.getAttribute('data'))
-    var student_dropdown_list = parsed_props.students.map(function (a) { return { id: a.netID, text: a.netID + "  -  " + a.name }});
-    var supervisor_dropdown_list = parsed_props.supervisors.map(function (a) { return { id: a.netID, text: a.netID + "  -  " + a.name }});
-    const props = { students: student_dropdown_list, supervisors: supervisor_dropdown_list }
-
-    if (element != null && props != null) {
-        const el = element
-        new Vue({
-            el,
-            render: h => h(Assign, { props }),
-        });
-    }
+  if (element != null && props != null) {
+    const el = element;
+    new Vue({
+      el,
+      render: h => h(Assign, { props })
+    });
+  }
 });
 
-document.addEventListener('turbolinks:load', () => {
-    var element = document.getElementById("todo");
-    if (element != null) {
-        const el = element;
-        var parsed_props = JSON.parse(element.getAttribute('data'));
-        parsed_props.items2.map((obj) => {
-            obj.icon_class = 'fas fa-book';
-            obj.icon_status = obj.color;
-            obj.eta = obj.eta.replace(/\T(.*)/, "");
-            obj.controls = [
-                {
-                    method: 'edit',
-                    icon_class: 'fas fa-pen'
-                },
-                {
-                    method: 'delete',
-                    icon_class: 'fas fa-trash'
-                }
-            ];
-        });
+document.addEventListener("turbolinks:load", () => {
+  var element = document.getElementById("todo");
+  if (element == null) return;
+  const el = element;
+  var parsed_props = JSON.parse(element.getAttribute("data"));
+  parsed_props.items2.map(obj => {
+    obj.icon_class = "fas fa-book";
+    obj.icon_status = obj.color;
+    obj.eta = obj.eta.replace(/\T(.*)/, "");
+    obj.controls = [
+      {
+        method: "edit",
+        icon_class: "fas fa-pen"
+      },
+      {
+        method: "delete",
+        icon_class: "fas fa-trash"
+      }
+    ];
+  });
 
-        var props = {
-            items: parsed_props.items2,
-        };
+  var props = {
+    items: parsed_props.items2
+  };
 
-        window.todo_vue = new Vue({
-            el,
+  window.todo_vue = new Vue({
+    el,
 
-            render: h => h(Timeline, { props }),
+    render: h => h(Timeline, { props }),
 
-            events: {
-            }
-        })
-    }
+    events: {}
+  });
 });
