e5df1cdafa673d750b2d397741100c1b8e1e526e
Nelson Chan <chakflying@hotmail.com>
2019-02-01 18:03:28 +0800

    Keep dialog box if create todo failed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -40,6 +40,7 @@ class TodosController < ApplicationController
     else
       @todo_list = Todo.where(department: current_user.department).or(Todo.where(department: nil)).order('eta ASC')
     end
+    @retry = true
     @departments_list = get_departments_list
     render 'index'
   end
@@ -87,7 +88,7 @@ class TodosController < ApplicationController
   end
 
   def olddb_todo_create(params)
-    @old_todo = OldTodo.create(issued_department: ((params[:department_id] != '') && !params[:department_id].nil? ? Department.find(params[:department_id]).sync_id : nil), status: 1, title: params[:title], time: params[:eta], description: params[:description])
+    @old_todo = OldTodo.create(issued_department: ((params[:department_id] != '') && !params[:department_id].nil? ? Department.find(params[:department_id]).sync_id : nil), status: 1, title: params[:title], scope: 1, time: params[:eta], description: params[:description])
     @old_todo.id
   end
 
