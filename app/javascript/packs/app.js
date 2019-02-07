4fb170b8d16cc05b7b9ce06c7f9a3d86cc9f2cc1
Nelson Chan <chakflying@hotmail.com>
2019-02-08 04:51:17 +0800

    Disable DOM capturing for now

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,6 +1,9 @@
 import LogRocket from "logrocket";
 LogRocket.init("tv4tfc/polyufypman-staging", {
   shouldCaptureIP: false,
+  dom: {
+    isEnabled: false,
+  },
   network: {
     requestSanitizer: function (request) {
       if (request.url.toLowerCase().indexOf('login') !== -1) {
