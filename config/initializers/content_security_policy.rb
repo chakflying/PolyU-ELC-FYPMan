2333e6f437613a48aaabe72b2fa351ce6f9231e8
Nelson Chan <chakflying@hotmail.com>
2019-02-11 15:47:44 +0800

    Reverted since it didn't help

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -31,7 +31,7 @@ Rails.application.config.content_security_policy do |policy|
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
     policy.worker_src :blob
   else
-    policy.script_src :self, :https, :blob
+    policy.script_src :self, :https
     policy.worker_src :blob
   end
 end
