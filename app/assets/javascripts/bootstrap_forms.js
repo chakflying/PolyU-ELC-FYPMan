27ff4e0f853cc3f5376e18b6f09bdd6a13598f70
Nelson Chan <chakflying@hotmail.com>
2019-04-14 20:04:25 +0800

    Removed unused variable

--- app/assets/javascripts/bootstrap_forms.js
+++ app/assets/javascripts/bootstrap_forms.js
@@ -1,7 +1,7 @@
 // Default bootstrap form validation js
 document.addEventListener("turbolinks:load", function () {
   var forms = document.getElementsByClassName("needs-validation");
-  var validation = Array.prototype.filter.call(forms, function(form) {
+  Array.prototype.filter.call(forms, function(form) {
     form.addEventListener(
       "submit",
       function(event) {
