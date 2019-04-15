a90cd5089aeea8fa0842d4644554ff2526115f7b
Nelson Chan <chakflying@hotmail.com>
2019-04-15 21:38:27 +0800

    Error handling for ajax department filter

--- app/assets/javascripts/bootstrap_forms.js
+++ app/assets/javascripts/bootstrap_forms.js
@@ -20,17 +20,26 @@ document.addEventListener("turbolinks:load", function() {
     autoclose: true
   });
 
-  $("#validation_uni").on("change", function () {
-    $("#validation_dp").parent().prepend('<span id="dp_load" style="right: 3em;position: absolute;padding-top: 7px;">&nbsp;<i class="fas fa-sync-alt fa-spin" style="font-size:80%;"></i></span>');
-    $.get("/departments_list_by_uni", { id: this.value }, "json").done(function (data) {
-      let selectbox = $("#validation_dp");
-      selectbox.empty();
-      var list = '<option value="" disabled selected hidden>Select Department...</option>';
-      for (var j = 0; j < data.length; j++) {
-        list += "<option value='" + encodeURI(data[j][1]) + "'>" + data[j][0].replace(/['"<>]+/g, '') + "</option>";
-      }
-      selectbox.html(list);
+  $("#validation_uni").on("change", function() {
+    if ($("#dp_load")) {
       $("#dp_load").remove();
-    });
+    }
+    $("#validation_dp")
+      .parent()
+      .prepend('<span id="dp_load" style="right: 3em;position: absolute;padding-top: 7px;">&nbsp;<i class="fas fa-sync-alt fa-spin" style="font-size:80%;"></i></span>');
+    $.get("/departments_list_by_uni", { id: this.value }, "json")
+      .done(function(data) {
+        let selectbox = $("#validation_dp");
+        selectbox.empty();
+        var list = '<option value="" disabled selected hidden>Select Department...</option>';
+        for (var j = 0; j < data.length; j++) {
+          list += "<option value='" + encodeURI(data[j][1]) + "'>" + data[j][0].replace(/['"<>]+/g, "") + "</option>";
+        }
+        selectbox.html(list);
+        $("#dp_load").remove();
+      })
+      .fail(function() {
+        $("#dp_load").append("&nbsp;<small>check your internet connection.</small>");
+      });
   });
 });
