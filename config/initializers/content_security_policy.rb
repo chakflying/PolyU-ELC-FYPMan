f177bb9c26cbb39b2655c66ce57974aae4d6d19d
Nelson Chan <chakflying@hotmail.com>
2019-08-19 11:15:36 +0800

    Fix CSP again

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -31,8 +31,7 @@ Rails.application.config.content_security_policy do |policy|
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
     policy.worker_src :self, :blob
   else
-    policy.script_src :self, 'https://cdn.logrocket.io'
-    policy.script_src :self, 'https://kit.fontawesome.com'
+    policy.script_src :self, 'https://cdn.logrocket.io', 'https://kit.fontawesome.com'
     policy.connect_src :self, 'https://*.logrocket.io'
     policy.worker_src :self, :blob
   end
